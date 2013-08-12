79761a23ae49a0caaf470b514ede344d55673260
p1nkc0de <yggdrasil343@gmail.com>
2013-08-12 13:03:15 -0400

    Initial upload
    There's actually a slightly further along version somewhere on my
    ThinkPad, but this will do for now, I'll submit the other changes later.

---
+++ roller.py
@@ -0,0 +1,80 @@
+import sys
+import httplib, urllib
+import re
+import random
+
+#	client = httplib.HTTPConnection('www.random.org:80')
+
+#	client.request('GET', '/integers/?num=10&min=1&max=100&col=1&base=10&format=plain&rnd=new')
+
+#	res = client.getresponse()
+
+#	rawns = map(int, res.read().split())
+#	print rawns
+
+#	client.request('GET', '/quota/?format=plain')
+#	print "Bits remaining: ", client.getresponse().read()
+#	client.close()
+
+def roll(multi, face, mod):
+	#just going with a rand implementation for now to conserve bandwidth
+	return [random.randint(1, face)+mod for x in range(multi)]
+
+def rollSum(args):
+	pass
+
+def rollCount(args):
+	pass
+
+def rollCountAbove(args):
+	pass
+
+def rollCountBelow(args):
+	pass
+
+def rollShow(args):
+	pass
+
+def rollSort(args):
+	pass
+
+def showHelp(args):
+	pass
+
+def unrecognizedInput(args):
+	pass
+
+def main():
+	rolex = re.compile('(\d*)d(\d+)([+-]\d+)?', re.I)
+	done = False
+	lastrs = 'roll'
+	lastroll = []
+	while(not done):
+		prompt = raw_input("[%s]>> " % lastrs)
+
+		if(prompt == 'bye'): break
+
+		matches = rolex.match(prompt)
+		if(matches):		
+			multi = int(matches.group(1) if matches.group(1) != '' else 1)
+			face = int(matches.group(2))
+			mod = int(matches.group(3) if matches.group(3) is not None else 0 )
+			lastroll = roll(multi, face, mod)
+			print lastroll
+			lastrs = "{0}d{1}{2:+}".format(multi, face, mod)
+		else:
+			raw_command = prompt.split()
+			command = raw_command[0]
+			{
+				'sum' 			: rollSum,
+				'count'			: rollCount,
+				'countabove'	: rollCountAbove,
+				'countbelow'	: rollCountBelow,
+				'show'			: rollShow,
+				'sort'			: rollSort,
+				'help'			: showHelp,
+			}.get(command, unrecognizedInput)(raw_command[1:])
+
+
+if __name__ == "__main__":
+	main()
